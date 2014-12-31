6ecfb609cbc270ed6e07221a9adfae3a6357a702
Sebastien Letelie <sebastien.letelie@gmail.com>
2014-12-31 16:01:53 +0100

    update seb

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,26 +1,27 @@
 "use strict";
 
-angularMovieApp.controller("homeController" ,function ($scope) {
+angularMovieApp.controller("homeController" ,function homeController($scope) {
 
-    $scope.user = 'Thierry LAU';
+    this.user = 'Sébastien Letélié';
 
 });
 
-angularMovieApp.controller("moviesController" ,function ($scope, $http) {
+angularMovieApp.controller("moviesController" ,function moviesController($scope, $http) {
+  var vm = this;
 
     $http.get('/server/api/movies').success(function(resp){
-        $scope.movies = resp;
+        vm.movies = resp;
     });
 
 });
 
-angularMovieApp.controller("movieFormController" ,function ($scope, $http) {
-
-    $scope.addMovie = function(movie){
+angularMovieApp.controller("movieFormController" ,function movieFormController($scope, $http) {
+  var vm = this;
+    vm.addMovie = function(movie){
 
         $http.post('/server/api/movies', movie)
             .success(function(){
-                $scope.movies.push(movie);
+                $scope.mo.movies.push(movie);
                 $scope.movie = {};
             })
             .error(function(resp){
@@ -30,4 +31,3 @@ angularMovieApp.controller("movieFormController" ,function ($scope, $http) {
     };
 
 });
-
